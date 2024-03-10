64c343b2ec104e0c827511de5b84f6e5cd5b3d62
jelemiekun - laptop <jeremieeden1946@gmail.com>
2024-03-11 00:40:39 +0800

    will code productModel functions

--- src/main/java/com/example/postearevised/Controllers/Additional/ProductController.java
+++ src/main/java/com/example/postearevised/Controllers/Additional/ProductController.java
@@ -1,4 +1,469 @@
 package com.example.postearevised.Controllers.Additional;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TouchEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class ProductController {
+
+    @FXML
+    private AnchorPane anchorPaneAppetizer;
+
+    @FXML
+    private AnchorPane anchorPaneBtnDone;
+
+    @FXML
+    private AnchorPane anchorPaneBtnEditPhoto;
+
+    @FXML
+    private AnchorPane anchorPaneCoffee;
+
+    @FXML
+    private AnchorPane anchorPaneCoolers;
+
+    @FXML
+    private AnchorPane anchorPaneEditPhoto;
+
+    @FXML
+    private AnchorPane anchorPaneIceCandyCups;
+
+    @FXML
+    private AnchorPane anchorPaneMilkTea;
+
+    @FXML
+    private AnchorPane anchorPaneRemovePhoto;
+
+    @FXML
+    private AnchorPane anchorPaneRepositionPhoto;
+
+    @FXML
+    private AnchorPane anchorPaneRepositionPhotoConfirmation;
+
+    @FXML
+    private AnchorPane anchorPaneUploadPhoto;
+
+    @FXML
+    private Label appetizerLabelPrice;
+
+    @FXML
+    private TextField appetizerTextFieldPrice;
+
+    @FXML
+    private ImageView btnEditProductDescription;
+
+    @FXML
+    private ImageView btnEditProductName;
+
+    @FXML
+    private Label coffeeLabelPrice;
+
+    @FXML
+    private RadioButton coffeeRadioBtnCold;
+
+    @FXML
+    private RadioButton coffeeRadioBtnHot;
+
+    @FXML
+    private ToggleGroup coffeeTemperature;
+
+    @FXML
+    private TextField coffeeTextFieldPrice;
+
+    @FXML
+    private ComboBox<?> comboBoxCategories;
+
+    @FXML
+    private Label coolersLabelLargePrice;
+
+    @FXML
+    private Label coolersLabelMediumPrice;
+
+    @FXML
+    private Label coolersLabelSmallPrice;
+
+    @FXML
+    private ToggleGroup coolersLiquidBase;
+
+    @FXML
+    private RadioButton coolersRadioBtnLarge;
+
+    @FXML
+    private RadioButton coolersRadioBtnMedium;
+
+    @FXML
+    private RadioButton coolersRadioBtnSmall;
+
+    @FXML
+    private RadioButton coolersRadioBtnTeaBase;
+
+    @FXML
+    private RadioButton coolersRadioBtnWaterBase;
+
+    @FXML
+    private ToggleGroup coolersSizes;
+
+    @FXML
+    private TextField coolersTextFieldLargePrice;
+
+    @FXML
+    private TextField coolersTextFieldMeidunPrice;
+
+    @FXML
+    private TextField coolersTextFieldSmallPrice;
+
+    @FXML
+    private Label iceCandyCupsLabelPrice;
+
+    @FXML
+    private TextField iceCandyCupsTextFieldPrice;
+
+    @FXML
+    private ImageView imgProduct;
+
+    @FXML
+    private ImageView imgRepositionPhotoCancel;
+
+    @FXML
+    private ImageView imgRepositionPhotoConfirm;
+
+    @FXML
+    private Label labelAddEditDone;
+
+    @FXML
+    private Label labelDescriptionWordCounter;
+
+    @FXML
+    private Label labelProductDescription;
+
+    @FXML
+    private Label labelProductName;
+
+    @FXML
+    private ToggleGroup milkTeaAddOns;
+
+    @FXML
+    private Label milkTeaLabelAddOnsPriceOne;
+
+    @FXML
+    private Label milkTeaLabelAddOnsPriceTwo;
+
+    @FXML
+    private Label milkTeaLabelLargePrice;
+
+    @FXML
+    private Label milkTeaLabelMediumPrice;
+
+    @FXML
+    private Label milkTeaLabelSmallPrice;
+
+    @FXML
+    private ToggleGroup milkTeaLiquidBase;
+
+    @FXML
+    private RadioButton milkTeaRadioBtnAddOnsOne;
+
+    @FXML
+    private RadioButton milkTeaRadioBtnAddOnsTwo;
+
+    @FXML
+    private RadioButton milkTeaRadioBtnLarge;
+
+    @FXML
+    private RadioButton milkTeaRadioBtnMedium;
+
+    @FXML
+    private RadioButton milkTeaRadioBtnSmall;
+
+    @FXML
+    private RadioButton milkTeaRadioBtnTeaBase;
+
+    @FXML
+    private RadioButton milkTeaRadioBtnWaterBase;
+
+    @FXML
+    private ToggleGroup milkTeaSizes;
+
+    @FXML
+    private TextField milkTeaTextFieldAddOnsOneName;
+
+    @FXML
+    private TextField milkTeaTextFieldAddOnsPriceOne;
+
+    @FXML
+    private TextField milkTeaTextFieldAddOnsPriceTwo;
+
+    @FXML
+    private TextField milkTeaTextFieldAddOnsTwoName;
+
+    @FXML
+    private TextField milkTeaTextFieldLargePrice;
+
+    @FXML
+    private TextField milkTeaTextFieldMediumPrice;
+
+    @FXML
+    private TextField milkTeaTextFieldSmallPrice;
+
+    @FXML
+    private TextArea textFieldProductDescription;
+
+    @FXML
+    private TextField textFieldProductName;
+
+    @FXML
+    void anchorPaneBtnDoneClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneBtnDoneTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneBtnEditPhotoClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneBtnEditPhotoTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneEditPhotoClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneEditPhotoTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneRemovePhotoClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneRemovePhotoTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneRepositionPhotoClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneRepositionPhotoTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneUploadPhotoClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void anchorPaneUploadPhotoTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void appetizerTextFieldPriceTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void btnEditProductDescriptionClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnEditProductDescriptionTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void btnEditProductNameClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void btnEditProductNameTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void coffeeRadioBtnColdClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void coffeeRadioBtnHotClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void coffeeTextFieldPriceTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void comboBoxCategoriesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void coolersRadioBtnLargeClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void coolersRadioBtnMediumClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void coolersRadioBtnSmallClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void coolersRadioBtnTeaBaseClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void coolersRadioBtnWaterBaseClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void coolersTextFieldLargePriceTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void coolersTextFieldMeidunPriceTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void coolersTextFieldSmallPriceTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void iceCandyCupsTextFieldPriceTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void imgRepositionPhotoCancelClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imgRepositionPhotoCancelTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void imgRepositionPhotoConfirmClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imgRepositionPhotoConfirmTouched(TouchEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaRadioBtnAddOnsOneClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaRadioBtnAddOnsTwoClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaRadioBtnLargeClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaRadioBtnMeidumClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaRadioBtnSmallClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaRadioBtnTeaBaseClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaRadioBtnWaterBaseClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaTextFieldAddOnsOneNameTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaTextFieldAddOnsPriceOneTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaTextFieldAddOnsPriceTwoTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaTextFieldAddOnsTwoNameTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaTextFieldLargePriceTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaTextFieldMediumPriceTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void milkTeaTextFieldSmallPriceTyping(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void textFieldProductDescriptionTyping(KeyEvent event) {
+
+    }
+
 }
